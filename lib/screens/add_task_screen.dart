6b71bb4562754e89a65200a5788a09b6b2b58f99
Nilesh <allmailtonilesh@gmail.com>
2021-05-07 23:07:35 +0530

    Update Task Screen UI

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -6,11 +6,44 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xff757575),
       child: Container(
+        padding: EdgeInsets.all(39.0),
         decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0))),
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 34.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            ElevatedButton(
+              onPressed: () => {},
+              child: Text(
+                'Add',
+                style: TextStyle(
+                    color: Colors.white, fontSize: 18.0, letterSpacing: 1.0),
+              ),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.lightBlueAccent)),
+            )
+          ],
+        ),
       ),
     );
   }
