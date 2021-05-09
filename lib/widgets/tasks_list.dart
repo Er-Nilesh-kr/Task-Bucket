a761795690839d0d33043dde2b84f64c2e64a427
Nilesh <allmailtonilesh@gmail.com>
2021-05-09 23:45:24 +0530

    Add task functionality dynamically

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/models/task.dart';
 import 'package:task_app/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  final List<Task> tasks;
+  TasksList(this.tasks);
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+            taskTitle: widget.tasks[index].name,
+            isChecked: widget.tasks[index].isDone,
+            checkboxCallback: (checkboxState) {
+              setState(
+                () {
+                  widget.tasks[index].toggleDone();
+                },
+              );
+            });
+      },
+      itemCount: widget.tasks.length,
     );
   }
 }
