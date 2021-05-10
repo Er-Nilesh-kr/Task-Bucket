e5fb0fb00eceb604a63b53a8b942138b47e209b7
Nilesh <allmailtonilesh@gmail.com>
2021-05-11 01:32:15 +0530

    Add task functions

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,3 +1,31 @@
-import 'package:flutter/foundation.dart';
+import 'dart:collection';
 
-class TaskData extends ChangeNotifier {}
+import 'package:flutter/foundation.dart';
+import 'package:task_app/models/task.dart';
+
+class TaskData extends ChangeNotifier {
+  List<Task> _tasks = [
+    Task(name: 'Buy demo1'),
+    Task(name: 'Demo2'),
+    Task(name: 'Demo3'),
+  ];
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+}
