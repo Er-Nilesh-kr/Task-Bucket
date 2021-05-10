6b68aff2862ebfc0cbb3fa6e9c7cc10cb5b95d46
Nilesh <allmailtonilesh@gmail.com>
2021-05-11 01:31:43 +0530

    Add provider data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:task_app/models/task_data.dart';
 import 'package:task_app/screens/tasks_screen.dart';
 
 void main() {
@@ -9,13 +11,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        textTheme: GoogleFonts.latoTextTheme(
-          Theme.of(context).textTheme,
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => TaskData(),
+      child: MaterialApp(
+        theme: ThemeData(
+          textTheme: GoogleFonts.latoTextTheme(
+            Theme.of(context).textTheme,
+          ),
         ),
+        home: TasksScreen(),
       ),
-      home: TasksScreen(),
     );
   }
 }
